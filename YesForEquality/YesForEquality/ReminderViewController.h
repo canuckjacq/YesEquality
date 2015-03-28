05690b853a0462a4328288869ec7d70804f6040f
rafammj <rafammj@gmail.com>
2015-03-28 11:30:22 +0000

    Added reminder files

---
+++ YesForEquality/YesForEquality/ReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderViewController.h
+//  YesForEquality
+//
+//  Created by rafaelajopia on 28/03/15.
+//  Copyright (c) 2015 YesForEquality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderViewController : UIViewController
+
+@end
