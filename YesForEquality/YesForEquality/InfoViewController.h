d4085294e31af127a8a3eadfe849ab17a3de8e6e
Adam Govan <adamgovan@Adams-MacBook-Air.local>
2015-03-28 11:43:16 +0000

    added info screen

---
+++ YesForEquality/YesForEquality/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  YesForEquality
+//
+//  Created by Adam Govan on 28/03/2015.
+//  Copyright (c) 2015 YesForEquality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
