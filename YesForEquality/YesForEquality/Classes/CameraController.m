248459901fc9e42ef2a201e9e001b077d0e642ee
Liam Dunne <liamodunne@mac.com>
2015-03-28 13:01:40 +0000

    Updated storyboard (layout all over the shop)

---
+++ YesForEquality/YesForEquality/Classes/CameraController.m
@@ -0,0 +1,21 @@
+//
+//  CameraController.m
+//  YesForEquality
+//
+//  Created by Liam Dunne on 28/03/2015.
+//  Copyright (c) 2015 YesForEquality. All rights reserved.
+//
+
+#import "CameraController.h"
+
+@implementation CameraController
+
+- (void)captureStillImage:(void (^)(UIImage *image, NSDictionary *metadata))completion{
+    
+}
+
+- (void)logSession{
+    
+}
+
+@end
--- YesForEquality/YesForEquality/ViewControllers/CameraViewController.m
+++ YesForEquality/YesForEquality/ViewControllers/CameraViewController.m
@@ -7,13 +7,22 @@
 //
 
 #import "CameraViewController.h"
+#import "CameraController.h"
 
 @interface CameraViewController ()
 @property (weak, nonatomic) IBOutlet UIView *cameraView;
 @property (weak, nonatomic) IBOutlet UIImageView *logoView;
+@property (weak, nonatomic) IBOutlet UIButton *menuButton;
 @property (weak, nonatomic) IBOutlet UIButton *flipCameraButton;
 @property (weak, nonatomic) IBOutlet UIButton *cameraButton;
 @property (weak, nonatomic) IBOutlet UIButton *shareButton;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *videoPreviewViewWidthConstraint;
+@property (assign, nonatomic) BOOL isDisplayingStillImage;
+@property (assign, nonatomic) BOOL usingFrontCamera;
+@property (strong, nonatomic) CameraController *cameraController;
+@property (strong, nonatomic) UIImageView *stillImageView;
+@property (nonatomic,strong) UIImage *renderedImage;
 
 @end
 
@@ -29,14 +38,111 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)didTapMenuButton:(id)sender {
 }
-*/
+- (IBAction)didTapFlipButton:(id)sender {
+}
+- (IBAction)didTapCameraButton:(id)sender {
+}
+- (IBAction)didTapInfoButton:(id)sender {
+}
+
+- (IBAction)didTapShareButton:(id)sender {
+}
+
+- (void)takePhoto{
+    if (!self.isDisplayingStillImage){
+        
+        self.shareButton.enabled = YES;
+        
+        [self.cameraController logSession];
+        
+        [self.cameraController captureStillImage:^(UIImage *image, NSDictionary *metatata){
+            
+            UIImage *outputImage = image;
+            if (self.usingFrontCamera) {
+                UIImage *mirroredImage = [UIImage imageWithCGImage:image.CGImage scale:1.0 orientation:UIImageOrientationLeftMirrored];
+                outputImage = mirroredImage;
+            }
+            
+            self.cameraController.previewLayer.hidden = YES;
+            
+            self.stillImageView = [[UIImageView alloc] initWithFrame:self.cameraView.bounds];
+            self.stillImageView.clipsToBounds = YES;
+            self.stillImageView.contentMode = UIViewContentModeScaleAspectFill;
+            self.stillImageView.image = outputImage;
+            [self.cameraView addSubview:self.stillImageView];
+            [self.cameraView sendSubviewToBack:self.stillImageView];
+            
+            CGSize size = self.cameraView.frame.size;
+            
+            UIGraphicsBeginImageContextWithOptions(size, false, 0);
+            CGContextRef context = UIGraphicsGetCurrentContext();
+            [self.cameraView.layer renderInContext:context];
+            self.renderedImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            
+            self.isDisplayingStillImage = YES;
+            
+            self.stillImageView.alpha = 0.0;
+            [UIView animateWithDuration:0.5 animations:^{
+                self.stillImageView.alpha = 1.0;
+            }];
+            
+        }];
+        
+    } else {
+        self.isDisplayingStillImage = NO;
+        [self removePreviewImageview];
+        self.shareButton.enabled = NO;
+    }
+
+}
+
+- (void)removePreviewImageview{
+    self.cameraController.previewLayer.hidden = NO;
+    [self.stillImageView removeFromSuperview];
+}
+
+
+- (void)saveImageToSavedPhotosAlbum:(UIImage*)image{
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    [self removePreviewImageview];
+}
+- (void)shareImageOnTwitter:(UIImage*)image{
+    SLComposeViewController* tweet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    tweet.completionHandler = ^(SLComposeViewControllerResult result){
+        switch (result) {
+            case SLComposeViewControllerResultCancelled:
+                break;
+            case SLComposeViewControllerResultDone:
+                break;
+        }
+    };
+    [tweet setInitialText:@"Vote Yes!"]; //The default text in the tweet
+    [tweet addImage:image]; //Add an image
+    [tweet addURL:[NSURL URLWithString:@"http://facebook.com"]]; //A url which takes you into safari if tapped on
+    [self presentViewController:tweet animated:YES completion: ^{
+        [self removePreviewImageview];
+    }];
+}
+- (void)shareImageOnFacebook:(UIImage*)image{
+    SLComposeViewController *facebookPost = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+    facebookPost.completionHandler = ^(SLComposeViewControllerResult result){
+        switch (result) {
+        case SLComposeViewControllerResultCancelled:
+                break;
+        case SLComposeViewControllerResultDone:
+                break;
+        }
+    };
+    [facebookPost setInitialText:@"Vote Yes!"]; //The default text in the tweet
+    [facebookPost addImage:image]; //Add an image
+    [facebookPost addURL:[NSURL URLWithString:@"http://facebook.com"]]; //A url which takes you into safari if tapped on
+    [self presentViewController:facebookPost animated:YES  completion:^{
+        [self removePreviewImageview];
+    }];
+}
+
 
 @end
